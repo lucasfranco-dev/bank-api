6b096b1817f15d03f495b23c87d9bea833a5308d
luczinn <lucfpscs@gmail.com>
2024-06-26 21:59:15 -0300

    feat: Add atributtes to Transaction Model

--- src/main/java/com/luczin/bankapi/models/Transaction.java
+++ src/main/java/com/luczin/bankapi/models/Transaction.java
@@ -1,11 +1,17 @@
 package com.luczin.bankapi.models;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.math.BigDecimal;
 
 @Entity(name = Transaction.TABLE_NAME)
 @Table(name = Transaction.TABLE_NAME)
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@EqualsAndHashCode(of = "id")
 public class Transaction {
     public final static String TABLE_NAME = "transactions";
 
@@ -13,9 +19,15 @@ public class Transaction {
     @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
 
+    @ManyToOne
+    @JoinColumn(name = "sender_id")
     private User sender;
 
+    @ManyToOne
+    @JoinColumn(name = "sender_id")
     private User receiver;
 
     private BigDecimal amount;
+
+    private Boolean reversed;
 }

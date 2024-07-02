429c8ef8baeac5ebd97401cc552d65221ab8978a
luczinn <lucfpscs@gmail.com>
2024-07-02 16:11:41 -0300

    feat: Create TransactionsRepository

--- src/main/java/com/luczin/bankapi/repositories/TransactionsRepository.java
+++ src/main/java/com/luczin/bankapi/repositories/TransactionsRepository.java
@@ -2,6 +2,8 @@ package com.luczin.bankapi.repositories;
 
 import com.luczin.bankapi.models.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TransactionsRepository extends JpaRepository<Transaction, String> {
 }

87c4b3dba8c3fd738c6d0fa31828fa1e8e628529
luczinn <lucfpscs@gmail.com>
2024-07-02 16:09:08 -0300

    feat: implementing transaction time system and creating constructors

--- src/main/java/com/luczin/bankapi/models/Transaction.java
+++ src/main/java/com/luczin/bankapi/models/Transaction.java
@@ -1,9 +1,11 @@
 package com.luczin.bankapi.models;
 
+import com.luczin.bankapi.dtos.TransactionDTO;
 import jakarta.persistence.*;
 import lombok.*;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 @Entity(name = Transaction.TABLE_NAME)
 @Table(name = Transaction.TABLE_NAME)
@@ -14,6 +16,7 @@ import java.math.BigDecimal;
 @EqualsAndHashCode(of = "id")
 public class Transaction {
     public final static String TABLE_NAME = "transactions";
+    public final static Integer LIMIT_DAYS_FOR_REFUND = 3;
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
@@ -29,5 +32,17 @@ public class Transaction {
 
     private BigDecimal amount;
 
-    private Boolean reversed;
+    private LocalDateTime timeStamp;
+
+    private Boolean refunded;
+
+    public Transaction(TransactionDTO transactionDTO){
+        this.sender = transactionDTO.sender();
+        this.receiver = transactionDTO.receiver();
+        this.amount = transactionDTO.amount();
+        this.refunded = false;
+        this.timeStamp = LocalDateTime.now();
+    }
+
+
 }

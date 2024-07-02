7846e47bd985483e9c5a0895f8a7e3e3ddadd3c8
luczinn <lucfpscs@gmail.com>
2024-07-02 16:11:23 -0300

    feat: Create User Repository

--- src/main/java/com/luczin/bankapi/repositories/UserRepository.java
+++ src/main/java/com/luczin/bankapi/repositories/UserRepository.java
@@ -2,6 +2,15 @@ package com.luczin.bankapi.repositories;
 
 import com.luczin.bankapi.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    public Optional<User> findUserById(Long id);
+    public List<User> findAllActiveUser();
+
 }

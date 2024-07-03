7731f2f8833bcc879f9b18cccbae07cee946dac6
luczinn <lucfpscs@gmail.com>
2024-07-03 12:13:09 -0300

    feat: adding syntax search method

--- src/main/java/com/luczin/bankapi/repositories/TransactionsRepository.java
+++ src/main/java/com/luczin/bankapi/repositories/TransactionsRepository.java
@@ -4,6 +4,10 @@ import com.luczin.bankapi.models.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TransactionsRepository extends JpaRepository<Transaction, String> {
+
+    public List<Transaction> findAllByRefundedFalse();
 }
